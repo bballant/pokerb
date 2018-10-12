c6cb0a016f23aeff786c90d1a265a67e44a4432c
bballant <brianballantine@gmail.com>
2018-10-11 23:54:54 -0400

    support for > 5 cards in hand etc

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Data.Time (getCurrentTime)
+import Data.List
+import System.Random
+import System.IO.Unsafe
 import Pokerb
 
 printTime = do
@@ -27,9 +30,23 @@ aTwoPair       = [Card Two Spades, Card Two Hearts, Card Jack Diamonds, Card Jac
 aPair          = [Card Two Spades, Card Two Hearts, Card Jack Diamonds, Card King Clubs, Card Queen Hearts]
 aHighCard      = [Card Two Spades, Card Five Hearts, Card Jack Diamonds, Card King Clubs, Card Queen Hearts]
 
+aRoyalFlush7    = [Card Two Clubs, Card Four Clubs, Card Ten Hearts, Card Jack Hearts, Card Queen Hearts, Card King Hearts, Card Ace Hearts]
+aStraightFlush7 = [Card Two Clubs, Card Four Clubs, Card Two Hearts, Card Three Hearts, Card Four Hearts, Card Five Hearts, Card Six Hearts]
+aFourOfAKind7   = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Two Hearts, Card Two Diamonds, Card Two Clubs, Card Queen Hearts]
+aFullHouse7     = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Two Hearts, Card Two Diamonds, Card King Clubs, Card King Hearts]
+aFlush7         = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Four Spades, Card Ace Spades, Card Jack Spades, Card Nine Spades]
+aStraight7      = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Three Hearts, Card Four Diamonds, Card Five Clubs, Card Six Hearts]
+aThreeOfAKind7  = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Two Hearts, Card Two Diamonds, Card King Clubs, Card Queen Hearts]
+aTwoPair7       = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Two Hearts, Card Jack Diamonds, Card Jack Clubs, Card Queen Hearts]
+aPair7          = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Two Hearts, Card Jack Diamonds, Card King Clubs, Card Queen Hearts]
+aHighCard7      = [Card Two Clubs, Card Four Clubs, Card Two Spades, Card Five Hearts, Card Jack Diamonds, Card King Clubs, Card Queen Hearts]
+
 testHandRank :: [Card] -> PokerHandRank -> String
 testHandRank hand expectedRank =
-  (show expectedRank) ++ ": " ++ (show $ handRank5 hand == expectedRank)
+  (show expectedRank) ++ ": " ++ (show $ fiveCardHandRank hand == expectedRank)
+
+randomHand n = take n . unsafePerformIO . shuffleList . nub $ aStraight ++ aRoyalFlush ++ aFullHouse7 ++ aPair7
+findHandsFromRandom n = findHands (randomHand n)
 
 runTests :: IO ()
 runTests = do
@@ -44,3 +61,17 @@ runTests = do
   putStrLn $ testHandRank aTwoPair       TwoPair
   putStrLn $ testHandRank aPair          APair
   putStrLn $ testHandRank aHighCard      HighCard
+
+runTests7 :: IO ()
+runTests7 = do
+  putStrLn "Running tests"
+  putStrLn $ testHandRank aRoyalFlush7    RoyalFlush
+  putStrLn $ testHandRank aStraightFlush7 StraightFlush
+  putStrLn $ testHandRank aFourOfAKind7   FourOfAKind
+  putStrLn $ testHandRank aFullHouse7     FullHouse
+  putStrLn $ testHandRank aFlush7         Flush
+  putStrLn $ testHandRank aStraight7      Straight
+  putStrLn $ testHandRank aThreeOfAKind7  ThreeOfAKind
+  putStrLn $ testHandRank aTwoPair7       TwoPair
+  putStrLn $ testHandRank aPair7          APair
+  putStrLn $ testHandRank aHighCard7      HighCard
--- src/Pokerb.hs
+++ src/Pokerb.hs
@@ -45,7 +45,7 @@ data PokerHandRank = HighCard
                    deriving (Eq, Ord, Show, Enum, Bounded)
 
 cardsForRank :: Rank -> [Card]
-cardsForRank r = map (\s -> Card r s) [Diamonds ..]
+cardsForRank r = map (\s -> Card r s) [Clubs ..]
 
 -- 52 cards, fresh out of the wrapper
 deckO'Cards :: [Card]
@@ -61,6 +61,8 @@ shuffleList as = do
 
 shuffledDeckOfCards = shuffleList deckO'Cards
 unsafeShuffledCardsOnce = unsafePerformIO (shuffleList deckO'Cards)
+
+mapcat f xs = xs >>= f
 
 headNTail :: [a] -> Int -> ([a], a, [a])
 headNTail xs n = let (y:ys) = drop n xs
@@ -85,6 +87,87 @@ groupAndSortOfAKind cards =  reverse . (sortBy lenSorter) . (groupBy sameSuit) .
   where lenSorter :: [a] -> [a] -> Ordering
         lenSorter l l' = compare (length l) (length l')
 
+cardIdentity :: [Card] -> [Card]
+cardIdentity c = c
+
+ranksForCards :: [Card] -> [Rank]
+ranksForCards cards = map (\(Card r _) -> r) cards
+
+suitsForCards :: [Card] -> [Suit]
+suitsForCards cards = map (\(Card _ s) -> s) cards
+
+sortedRanksForCards :: [Card] -> [Rank]
+sortedRanksForCards cards = sort . ranksForCards $ cards
+
+ensureLengthFive :: [a] -> [a]
+ensureLengthFive cards = if length cards /= 5
+                         then error "Rotten"
+                         else cards
+
+isAtLeastNOfAKind :: [Card] -> Int -> Bool
+isAtLeastNOfAKind cards n = (length $ head groupedSortedCards) >= n
+  where groupedSortedCards = groupAndSortOfAKind cards
+
+isSecondPair :: [Card] -> Bool
+isSecondPair cards = (length . head . tail $ groupedSortedCards) >= 2
+  where groupedSortedCards = groupAndSortOfAKind cards
+
+isAFlush :: [Card] -> Bool
+isAFlush cards = all (\s -> s == head suits) suits
+  where suits = suitsForCards $ ensureLengthFive cards
+
+isAStraight :: [Card] -> Bool
+isAStraight cards = take 5 [(head ranks) ..] == take 5 ranks
+  where ranks = sortedRanksForCards $ ensureLengthFive cards
+
+isAHighStraight :: [Card] -> Bool
+isAHighStraight cards = head ranks == Ten && isAStraight cards
+  where ranks = sortedRanksForCards $ ensureLengthFive cards
+
+-- assumes 5 cards in the hand
+fiveCardHandRank :: [Card] -> PokerHandRank
+fiveCardHandRank cards
+  | length cards /= 5                               = error "Rotten"
+  | isAHighStraight cards     && isAFlush cards     = RoyalFlush
+  | isAStraight cards         && isAFlush cards     = StraightFlush
+  | isAtLeastNOfAKind cards 4                       = FourOfAKind
+  | isAtLeastNOfAKind cards 3 && isSecondPair cards = FullHouse
+  | isAFlush cards = Flush
+  | isAStraight cards                               = Straight
+  | isAtLeastNOfAKind cards 3                       = ThreeOfAKind
+  | isAtLeastNOfAKind cards 2 && isSecondPair cards = TwoPair
+  | isAtLeastNOfAKind cards 2                       = APair
+  | otherwise                                       = HighCard
+
+combinationOfN :: Eq a => Int -> [a] -> [[a]]
+combinationOfN n cs =
+  if n == 1 then (group cs)
+  else cs >>= \c -> map (c:) (combinationOfN (n-1) cs)
+
+uniqueCombinationsOfN :: Ord a => Int -> [a] -> [[a]]
+uniqueCombinationsOfN n cs =
+  nub . (map sort) . filter ((n==) . length . nub) $ combs
+  where combs = combinationOfN n cs
+
+findHands cards = reverse . sort . (map fiveCardHandRank) . uniqueCombinationsOfN 5 $ cards
+
+
+-------------------------- old
+
+combinationOfNCards' :: Eq a => Int -> [a] -> [[a]]
+combinationOfNCards' n cards =
+  if n == 1 then (group cards)
+  else combinationStep [] cards (combinationOfNCards' (n-1) cards)
+  where combinationStep :: [[a]] -> [a] -> [[a]] -> [[a]]
+        combinationStep acc (c:cs) prevCombs =
+          if length cs == 0 then acc
+          else combinationStep (acc ++ map (c:) prevCombs) cs prevCombs
+
+uniqueCombinationsOfN' :: Ord a => Int -> [a] -> [[a]]
+uniqueCombinationsOfN' n cs =
+  nub . (map sort) . filter ((n==) . length . nub) $ combs
+  where combs = combinationOfNCards' n cs
+
 -- assumes 5 cards in the hand
 handRank5 :: [Card] -> PokerHandRank
 handRank5 cards
@@ -108,5 +191,22 @@ handRank5 cards
     ranks           = map (\(Card r _) -> r) sorted
     suits           = map (\(Card _ s) -> s) sorted
     isAFlush        = all (\s -> s == head suits) suits
-    isAStraight     = take (length cards) [(head ranks) ..] == ranks
+    isAStraight     = hasAStraight cards
     isAHighStraight = (head ranks == Ten) && isAStraight
+
+hasAStraightOld :: [Card] -> Bool
+hasAStraightOld cards = any isA5CardStraight (fiveCardSeqs [] sortedCards)
+  where sortedCards = sort cards
+        isA5CardStraight :: [Card] -> Bool
+        isA5CardStraight cs = take 5 [(head ranks) ..] == ranks
+          where ranks = map (\(Card r _) -> r) cs
+        fiveCardSeqs :: [[Card]] -> [Card] -> [[Card]]
+        fiveCardSeqs acc cs = if length cs < 5 then acc
+                              else fiveCardSeqs ((take 5 cs) : acc) (tail cs)
+
+hasAStraight :: [Card] -> Bool
+hasAStraight cards = go (sort cards)
+  where go cs
+         | length cs < 5                   = False
+         | isAStraight (take 5 cs) = True
+         | otherwise                       = go (tail cards)
